64e0f15acc24174d7c44326f162af48d891d7698
Keming He <keminghe.career@gmail.com>
2024-06-02 15:07:59 -0400

    Migrated to Biome, added Husky.

--- cucumber.js
+++ cucumber.js
@@ -1,20 +1,20 @@
-// ./cucumber.js
+// ./cucumber.cjs
 //
 // Config file for cucumber-js.
 //   Note: cucumber is written in commonjs,
 //   this will NOT work if changed to cucumber.config.json.
-let common = [
-  // Specify feature files dir.
-  'features/**/*.feature',
-
-  // Load: ts modules, step defs, and formatters.
-  '--require-module ts-node/register',
-  '--require step-definitions/**/*.steps.ts',
-  '--format progress-bar',
-  '--format @cucumber/pretty-formatter',
-].join(' ');
-
-// Export common config as default.
-module.exports = {
-  default: common,
+const config = {
+  paths: ['__tests__/__features__/**/*.feature',],
+  require: ['__tests__/__features__/**/*.steps.ts',],
+  requireModule: ['ts-node/register',],
+  format: [
+    'summary',
+    'progress-bar',
+    '@cucumber/pretty-formatter',
+  ],
+  formatOptions: { snippetInterface: 'async-await' },
 };
+
+module.exports = {
+ default: config
+}
--- src/calculator-example.test.ts
+++
@@ -1,8 +0,0 @@
-// ./src/calculator-example.test.ts
-import { Calculator } from './calculator-example';
-
-test('single digit addition test case', () => {
-  const newUid = 1;
-  const testCalc = new Calculator(newUid);
-  expect(testCalc.addNum(1, 2)).toBe(3);
-});
